753ccca098ce6e6b809fe9ca3367d3f42b05030b
what's for dinner <cruelangeleo7@gmail.com>
2020-04-12 16:44:31 +1000

    no longer need random env prefix

--- .github/terraform/main.tf
+++ .github/terraform/main.tf
@@ -4,14 +4,8 @@ terraform {
   }
 }
 
-resource "random_string" "workspace_override" {
-    length = 8
-    special = false
-}
-
 module "github_action_testing" {
   source     = "github.com/whatsfordinner/github-actions-testing//infra"
-  env_prefix = random_string.workspace_override.result
 }
 
 output "api_url" {
